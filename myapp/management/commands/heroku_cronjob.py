62989bf10e025a238d2d68a4af2d4ea7116f32c8
makrand55 <makrand.mandhare@dynamisch.co>
2025-03-31 18:44:36 +0530

    command updated

--- myapp/management/commands/heroku_cronjob.py
+++ myapp/management/commands/heroku_cronjob.py
@@ -1,8 +1,13 @@
+import logging
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
-        today=timezone.now()
-        print("helou cron job executing at {}",today)
+        today = timezone.now()
+        log_message = f"helou cron job executing at {today}"
+        logger.info(log_message)
+        print(log_message) 
