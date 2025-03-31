e9bf528200a044aa0592ddca334a1ffd4eca9513
makrand55 <makrand.mandhare@dynamisch.co>
2025-03-31 17:09:58 +0530

    added heroku base command

---
+++ myapp/management/commands/heroku_cronjob.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+class HerokuCron(BaseCommand):
+    
+    def handle(self, *args, **options):
+        today=timezone.now()
+        print("helou cron job executing at {}",today)
